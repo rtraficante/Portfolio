715837c69d7aca2e53e4fd5f5bc2a6fba0be05ec
RTraficante <rtraficante22@gmail.com>
2022-07-20 16:23:38 -0400

    initial commit

---
+++ blue/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Ryan Traficante Portfolio'"; //letter subject
+	$to = 'rtraficante22@gmail.com'; // Enter Your E-mail
+	$from = 'admin@ryantraficante.com'; // Admin e-mail
+	$fromName = 'Portfolio'; // Your company name
+	$charset = 'UTF-8';
+?>
